85a4956f9cac9dedeff3f46601bd947ab56e2d94
Nicolò Sponziello <37869067+nicolosponziello@users.noreply.github.com>
2021-11-02 17:32:40 +0100

    device: add set position

--- iot/device.py
+++ iot/device.py
@@ -1,27 +1,33 @@
-import battery
-import processor
-import sensor
-import network
+from iot.sensor import Sensor
+from iot.processor import Processor
+from iot.sensor import Sensor
+from iot.network import Network
+from iot.battery import Battery
 
 class Device:
     def __init__(self, sensor, network, battery, sensor_id):
-        self.sensor = sensor
-        self.network = network
-        self.battery = battery
+        self.sensor: Sensor = sensor
+        self.network: Network = network
+        self.battery: Battery = battery
         self.sensor_id = sensor_id
         self.x = 0
         self.y = 0
         self.z = 0
+
+    def set_position(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
     def get_total_cost(self):
         return self.sensor.cost + self.battery.cost + self.network.cost
 
     def measure(self, real_co2):
         # Inside sensor we should add error
-        if not self.is_empty():
-            return None
+        if not self.battery.is_empty():
+            return 0
         power_consumed = self.sensor.power
-        powerconsumed += network.power
+        #power_consumed += network.power
         self.battery.discharge(power_consumed)
         return self.sensor.measure(real_co2)
 
--- iot/network.py
+++ iot/network.py
@@ -1,2 +1,3 @@
 class Network:
+    pass
     
