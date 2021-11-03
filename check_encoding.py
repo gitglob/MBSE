45650d32aa3cdbd2d1006e3e3410f72833a186d5
gitglob <pgrigor22@gmail.com>
2021-11-03 20:27:05 +0100

    removed unnecessary files

--- check_encoding.py
+++
@@ -1,7 +0,0 @@
-# import the chardet library
-import chardet 
-
-# use the detect method to find the encoding
-# 'rb' means read in the file as binary
-with open("wind_speed_month_days.csv", 'rb') as file:
-    print(chardet.detect(file.read()))
