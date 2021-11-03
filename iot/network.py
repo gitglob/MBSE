95fe2a7d964585cde4f430b48e526fc60aee62da
Nicolò Sponziello <37869067+nicolosponziello@users.noreply.github.com>
2021-11-03 17:21:30 +0100

    network: add basic power usage implementation

--- iot/network.py
+++ iot/network.py
@@ -1,3 +1,26 @@
 class Network:
-    pass
+    def __init__(self, power_usage, time_per_msg):
+        self.power_usage = power_usage
+        self.recv_buffer = []
+        self.time_per_msg = time_per_msg
+        self.power_consumed = 0.0
+
+    def receive_data(self, msg):
+        self.recv_buffer.append(msg)
+        self.power_consumed = self.power_consumed + self.power_usage * self.time_per_msg
     
+    def empty_buffer(self):
+        for msg in self.recv_buffer:
+            self.send_msg(msg)
+        self.recv_buffer.clear()
+    
+    def send_msg(self, msg):
+        self.power_consumed = self.power_consumed + self.power_usage * self.time_per_msg
+
+    def get_power_consumed(self):
+        value = self.power_consumed
+        self.power_consumed = 0.0
+        return value
+
+class NetworkList:
+    LOWRA = Network(60, 2.8*10**-6) #mAh
