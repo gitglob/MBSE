52e00f6c2b8e598d537250a75fec43fb0b5b4915
Konstantinos Spyrikos <kspyrikos@gmaIl.com>
2021-11-25 12:13:24 +0100

    implemented gui with main.py

--- gui/gui.py
+++ gui/gui.py
@@ -4,194 +4,180 @@ import tkinter as tk
 
 ''' DEFAULT VALUES FOR THE SIMULATION
 
-TIME_TO_RUN     = 3600*24*3 # 1 day
-SENSOR_DISTANCE = 30 # 30 meters => 6 blocks
-SENSOR_PERIOD   = 1800 # every 30 minutes
+TIME_TO_RUN     = 3600*24*1 # 1 day
+SENSOR_DISTANCE = 15 # 15 meters => 3 blocks
+SENSOR_PERIOD   = 3600 # every 1 hour
 SENSOR_STATIC   = True
-SAVE_PLOTS      = False
+SAVE_PLOTS      = True
 DEBUG           = False
 
 '''
 
 class GUI:
 
-    
     def __init__(self, master, canvas):
         self.master = master
-        self.frame = tk.Frame(self.master, bg = 'black')
-        self.startButton = tk.Button(self.master, text = 'START the Simulation', padx = 10, pady = 10, 
-                                    command = self.start_simulation, font=('Helvetica', 10, 'bold'))
-        
-        self.quitButton = tk.Button(self.master, text = 'QUIT', padx = 10, pady = 10, 
-                                    command = self.close_windows, font=('Helvetica', 10, 'bold'))
-        
-       
-        self.parameters_button = tk.Button(self.master, text = 'Set Parameters', padx = 10, pady = 10, 
-                                    command = self.set_parameters,  font=('Helvetica', 10, 'bold'))
-       
+        self.frame = tk.Frame(self.master, bg='black')
+        self.startButton = tk.Button(self.master, text='START the Simulation', padx=10, pady=10,
+                                     command=self.start_simulation, font=('Helvetica', 10, 'bold'))
+
+        self.quitButton = tk.Button(self.master, text='QUIT', padx=10, pady=10,
+                                    command=self.close_windows, font=('Helvetica', 10, 'bold'))
+
+        self.parameters_button = tk.Button(self.master, text='Set Parameters', padx=10, pady=10,
+                                           command=self.set_parameters,  font=('Helvetica', 10, 'bold'))
+
         self.canvas = canvas
-        self.startButton_window = self.canvas.create_window(360, 645, window = self.startButton )
-        self.parameters_window = self.canvas.create_window(510, 645, window = self.parameters_button )
-        self.quitButton_window = self.canvas.create_window(605, 645, window = self.quitButton )
-       
+        self.startButton_window = self.canvas.create_window(
+            360, 645, window=self.startButton)
+        self.parameters_window = self.canvas.create_window(
+            510, 645, window=self.parameters_button)
+        self.quitButton_window = self.canvas.create_window(
+            605, 645, window=self.quitButton)
+
         self.days = 1
-        self.sensors_distance = 30
-        self.sensors_period = 1800
-        self.sensors_movement = True # True = 'static'
-        self.save_plots = False
+        self.sensors_distance = 15
+        self.sensors_period = 3600
+        self.sensors_movement = True  # True = 'static'
+        self.save_plots = True
         self.debug = False
         self.frame.pack()
-        
+
 
 #%%     ##################### PARAMETERS #####################
-    def set_parameters(self): 
-    
+    def set_parameters(self):
+
         self.parameters = tk.Toplevel()
         parameters = self.parameters
         parameters.title("Simulation Parameters")
         parameters.geometry("450x350")
-        button = tk.Button(parameters, text = "THE PARAMETERS ARE SET !!", command = self.close_windows_parameters, 
-                                    padx=10, pady=10, bd = 5, font=('Helvetica', 18, 'bold'))
+        button = tk.Button(parameters, text="THE PARAMETERS ARE SET !!", command=self.close_windows_parameters,
+                           padx=10, pady=10, bd=5, font=('Helvetica', 18, 'bold'))
         button.grid(row=10, column=1)
-        
-        
+
         #%%     SAVE PLOTS
-        
+
         self.save_plots2 = tk.BooleanVar()
-        self.s_p = tk.Checkbutton(parameters, text='Save Plots of the Simulation', variable = self.save_plots2,
-                            onvalue = True, offvalue = False, command = self.get_check_value_save_plots)
-        self.s_p.grid(row=1, column=1,sticky="nw")
-        
-        
+        self.s_p = tk.Checkbutton(parameters, text='Save Plots of the Simulation', variable=self.save_plots2,
+                                  onvalue=True, offvalue=False, command=self.get_check_value_save_plots)
+        self.s_p.grid(row=1, column=1, sticky="nw")
+
         #%%     SENSORS MOVEMENT
-        
+
         self.sensors_movement2 = tk.BooleanVar()
-        self.s_m = tk.Checkbutton(parameters, text='Sensors Movement', variable = self.sensors_movement2,
-                            onvalue = True, offvalue = False, command = self.get_check_value_sensors_movement)
-        self.s_m.grid(row=2, column=1,sticky="nw",  pady=5)
-        
-        
+        self.s_m = tk.Checkbutton(parameters, text='Sensors Movement', variable=self.sensors_movement2,
+                                  onvalue=True, offvalue=False, command=self.get_check_value_sensors_movement)
+        self.s_m.grid(row=2, column=1, sticky="nw",  pady=5)
+
         #%%     DEBUG
 
         self.debug2 = tk.BooleanVar()
-        self.d = tk.Checkbutton(parameters, text='Debug', variable = self.debug2,
-                            onvalue = True, offvalue = False, command = self.set_debug)
-        self.d.grid(row=3, column=1,sticky="nw",  pady=5)
-
+        self.d = tk.Checkbutton(parameters, text='Debug', variable=self.debug2,
+                                onvalue=True, offvalue=False, command=self.set_debug)
+        self.d.grid(row=3, column=1, sticky="nw",  pady=5)
 
         #%%     DAYS OF SIMULATION
 
-        self.e = tk.Entry(self.parameters, width = 5, font=('Helvetica', 18, 'bold'),
-                      textvariable = tk.IntVar())
+        self.e = tk.Entry(self.parameters, width=5, font=('Helvetica', 18, 'bold'),
+                          textvariable=tk.IntVar())
         self.e.grid(row=5, column=1, sticky="nw", padx=5, pady=5)
-        
-        button1 = tk.Button(self.parameters, text = "Set the Days for the Simulation", command = self.set_days)
+
+        button1 = tk.Button(
+            self.parameters, text="Set the Days for the Simulation", command=self.set_days)
         button1.grid(row=5, column=1, sticky="ne", padx=5, pady=5)
-        
-        
+
         #%%    SENSORS PERIOD
 
-        self.e2 = tk.Entry(self.parameters, width = 5, font=('Helvetica', 18, 'bold'),
-                      textvariable = tk.IntVar())
+        self.e2 = tk.Entry(self.parameters, width=5, font=('Helvetica', 18, 'bold'),
+                           textvariable=tk.IntVar())
         self.e2.grid(row=6, column=1, sticky="nw", padx=5, pady=5)
-        
-        button2 = tk.Button(self.parameters, text = "Set the Sensors Sampling Period in Secs", command = self.set_sensors_period)
+
+        button2 = tk.Button(
+            self.parameters, text="Set the Sensors Sampling Period in Secs", command=self.set_sensors_period)
         button2.grid(row=6, column=1, sticky="ne", padx=5, pady=5)
 
-    
         #%%     SENSORS DISTANCE
 
-        self.e1 = tk.Entry(self.parameters, width = 5, font=('Helvetica', 18, 'bold'),
-                      textvariable = tk.IntVar())
+        self.e1 = tk.Entry(self.parameters, width=5, font=('Helvetica', 18, 'bold'),
+                           textvariable=tk.IntVar())
         self.e1.grid(row=7, column=1, sticky="nw", padx=5,  pady=5)
-        
-        button2 = tk.Button(self.parameters, text = "Set the Distance of the Sensors in Meters", command = self.set_sensors_distance)
+
+        button2 = tk.Button(
+            self.parameters, text="Set the Distance of the Sensors in Meters", command=self.set_sensors_distance)
         button2.grid(row=7, column=1, sticky="ne", padx=5, pady=5)
 
         parameters.mainloop()
-        
-        
+
         #%%     FUNCTIONS FOR THE GUI
-        
-        
+
     def get_check_value_sensors_movement(self):
         self.sensors_movement = self.sensors_movement2.get()
-            #print('na malaka')
-        
+        #print('na malaka')
+
     def get_check_value_save_plots(self):
         self.save_plots = self.save_plots2.get()
-        
-        
+
     def set_days(self):  # THIS IS FOR THE SET DAYS BUTTON IN PARAMETERS
         self.days = self.e.get()
-        
+
     def set_sensors_distance(self):
         self.sensors_distance = self.e1.get()
-        
+
     def set_sensors_period(self):
         self.sensors_period = self.e2.get()
-        
+
     def set_debug(self):
         self.debug = self.debug2.get()
-        
-        
+
     def close_windows_parameters(self):  # THIS IS FOR THE PARAMETERS BUTTON
         self.parameters.destroy()
-        
-        
+
     def close_windows(self):  # THIS IS FOR THE QUIT BUTTON
         self.days = 0
         self.master.destroy()
-    
-    def start_simulation(self): # THIS IS FOR THE SET DAYS BUTTON
-        
+
+    def start_simulation(self):  # THIS IS FOR THE SET DAYS BUTTON
+
         if int(self.days) != 0:
             self.master.destroy()
-            
-            
-        
+
+
 #%% #############  NOT USED OBJECTS  ###############
 
 class Parameters():
     def __init__(self):
         super().__init__(self)
-        
+
         self.geometry('300x100')
         self.title('Toplevel Window')
 
-        button2 = tk.Button(self,text='Close',command=self.destroy)
+        button2 = tk.Button(self, text='Close', command=self.destroy)
         button2.pack()
-    
 
     def close_windows(self, master):
         # e = tk.Entry(root)
         # e.pack()
-        e = tk.Entry(master, width = 50)
+        e = tk.Entry(master, width=50)
         e.pack()
         self.master.destroy()
-        
+
     def new_window(self):
         self.newWindow = tk.Toplevel(self.master)
         self.app = Start(self.newWindow)
-        
-      
-        
+
     def on_button_press(entry):
         on_button_press.value = entry.get()
         entry.quit()
+
 
 class Quit:
     def __init__(self, master):
         self.master = master
         self.frame = tk.Frame(self.master)
-        self.quitButton = tk.Button(self.frame, text = 'Quit', padx = 60, pady = 40, 
-                                    command = self.close_windows, bg='red', fg='black', bd=5, font=('Helvetica', 18, 'bold'))
+        self.quitButton = tk.Button(self.frame, text='Quit', padx=60, pady=40,
+                                    command=self.close_windows, bg='red', fg='black', bd=5, font=('Helvetica', 18, 'bold'))
         self.quitButton.pack(side=tk.RIGHT)
         self.frame.pack()
-        
 
-        
     def close_windows(self):
         self.master.destroy()
-
-
--- main.py
+++ main.py
@@ -17,17 +17,17 @@ from gui.gui import GUI
 import tkinter as tk
 
 
-
 results_path = "figures\\results\\results.csv"
-folders = ['co2_comparison', 'co2_diffusion', 'co2_normalized_acc', 'co2_rain_effect', 'co2_timeseries', 'co2_trees_effect', 'co2_wind_effect', 'results']
+folders = ['co2_comparison', 'co2_diffusion', 'co2_normalized_acc', 'co2_rain_effect',
+           'co2_timeseries', 'co2_trees_effect', 'co2_wind_effect', 'results']
 try:
     os.mkdir("figures")
 except FileExistsError:
-        pass 
-    
+    pass
+
 for folder in folders:
     try:
-        os.mkdir("figures\\"+ folder)
+        os.mkdir("figures\\" + folder)
     except FileExistsError:
         pass
 
@@ -37,57 +37,71 @@ def debug(*args):
     if DEBUG:
         print(*args)
 
+
 def main():
-    #%% GUI 
+    #%% GUI
     root = tk.Tk()
     root.geometry("971x828")
     root.title("AIR POLLUTION SIMULATION")
-    bg = tk.PhotoImage(file = "pollution.png")
-    my_canvas = tk.Canvas(root, width = 971, height = 828)
+    bg = tk.PhotoImage(file="pollution.png")
+    my_canvas = tk.Canvas(root, width=971, height=828)
     my_canvas.pack()
-    my_canvas.create_image(0, 0, image = bg, anchor = "nw")
-    my_canvas.create_text(470, 584, text= "Air Pollution Simulation", font=("Helvetica", 50), fill="white")
+    my_canvas.create_image(0, 0, image=bg, anchor="nw")
+    my_canvas.create_text(470, 584, text="Air Pollution Simulation", font=(
+        "Helvetica", 50), fill="white")
     app = GUI(root, my_canvas)
     root.mainloop()
-    
-#%%   
+
+#%%
     # pprint(vars(app))
     running_time_in_days = int(app.days)
     SAVE_PLOTS = app.save_plots
-    if SAVE_PLOTS:
-        os.makedirs(os.path.join('figures', 'co2_timeseries'), exist_ok=True)
     SENSOR_DISTANCE = int(app.sensors_distance)
     REAL_C02_PERIOD = int(app.sensors_period)
     SENSOR_STATIC = int(app.sensors_movement)
     DEBUG = app.debug
+
+    results_path = "figures\\results\\results.csv"
+    folders = ['co2_comparison', 'co2_diffusion', 'co2_normalized_acc', 'co2_rain_effect',
+               'co2_timeseries', 'co2_trees_effect', 'co2_wind_effect', 'results']
+    try:
+        os.mkdir("figures")
+    except FileExistsError:
+        pass
+
+    if SAVE_PLOTS:
+        for folder in folders:
+            try:
+                os.mkdir("figures/" + folder)
+            except FileExistsError:
+                pass
 
     if DEBUG:
         print("TIME TO RUN:", running_time_in_days,
               "\nSENSORS DISTANCE IN METERS:", SENSOR_DISTANCE,
               "\nSENSORS SAMPLING PERIOD IN SECONDS", REAL_C02_PERIOD,
               "\nSAVE_PLOTS:", SAVE_PLOTS)
-              
-    
-    
-    TIME_TO_RUN = running_time_in_days*3600*24
+
+    TIME_TO_RUN = running_time_in_days * 3600 * 24
     if TIME_TO_RUN != 0:
         city = Grid()
         print("Our city is a {} grid".format([len(city.grid3d), len(city.grid3d[0]),
-            len(city.grid3d[0][0])]))
-    
+                                              len(city.grid3d[0][0])]))
+
         vis.visualize_3d_grid(city)
-    
+
         # extract the tree cells
-        trees, roads, emptys, emptys_0 = pre.extract_trees_roads_empty_blocks(city)
-    
-        sensor_manager = SensorManager(city, REAL_C02_PERIOD)
+        trees, roads, emptys, emptys_0 = pre.extract_trees_roads_empty_blocks(
+            city)
+
+        sensor_manager = SensorManager(city, SENSOR_PERIOD)
         sensor_manager.distribute_sensors(SENSOR_DISTANCE)
         sensor_number = sensor_manager.get_sensors_count()
         print("Placed " + str(sensor_number) + " sensors")
-    
+
         # visualiza the sensor placement
         vis.visualize_sensor(city, sensor_manager.devices)
-    
+
         # run the simulation - Note: Every iteration is 1 second
         sec = -1
         wind_speed_duration = 0
@@ -97,152 +111,170 @@ def main():
         real_values = []
         measured_values = []
         total_co2 = 0
-        print("Running simulation for {} days (this might take a while) ... \n\n".format(TIME_TO_RUN/3600/24))
-    
+        print("Running simulation for {} days (this might take a while) ... \n\n".format(
+            TIME_TO_RUN / 3600 / 24))
+
+        # data for dataframe
+        data = {
+            'measured_co2/cell': [],
+            'real_co2/cell': [],
+            'timeframe': [],
+            'sec': []
+        }
+
         while True:
             sec += 1
-    
-            date = str((f.sec_to(sec, "hour")%24)) + ":" + str(f.sec_to(sec, "minute")%60) + ":" + str(sec%60) + " - " + str(f.sec_to(sec, "day")%30 + 1) + "/" + str(f.sec_to(sec, "month")%12 + 1) + "/" + str(f.sec_to(sec, "year") + 1)
+
+            date = str((f.sec_to(sec, "hour") % 24)) + ":" + str(f.sec_to(sec, "minute") % 60) + ":" + str(sec % 60) + " - " + str(
+                f.sec_to(sec, "day") % 30 + 1) + "/" + str(f.sec_to(sec, "month") % 12 + 1) + "/" + str(f.sec_to(sec, "year") + 1)
             # print the date every day
-            if sec%86400 == 0:
+            if sec % 86400 == 0:
                 print("\nDate: ", date)
-    
-    
+
             # every 6 hours generate new positions for cars
-    
-            if sec%21600 == 0:
-               # debug("Hour: ", f.sec_to(sec, "hour")%24)
-                current_time = f.calculate_tz(f.sec_to(sec, "hour")%24)
-                cars = f.generate_cars(city, roads, time=current_time, max_cars=5000)
+
+            if sec % 21600 == 0:
+                print("Hour: ", f.sec_to(sec, "hour") % 24)
+                current_time = f.calculate_tz(f.sec_to(sec, "hour") % 24)
+                cars = f.generate_cars(
+                    city, roads, time=current_time, max_cars=5000)
                 # vis.visualize_cars(city, cars)
-    
+
             # cars generate co2 every minute
             if sec % 600 == 0:
                 f.generate_co2(cars, city, 600)
-    
-    
+
             # every hour apply the wind effect and the trees effect
             if sec % 600 == 0:
-                #every one hour visualize co2
+                # every one hour visualize co2
                 if SAVE_PLOTS:
-                    vis.visualize_co2(city, mesh=False, d=0, wind_direction=wind_direction, wind_speed=wind_speed, date=date)
-    
+                    vis.visualize_co2(
+                        city, mesh=False, d=0, wind_direction=wind_direction, wind_speed=wind_speed, date=date)
+                    vis.visualize_co2(
+                        city, mesh=False, d=3, wind_direction=wind_direction, wind_speed=wind_speed, date=date)
+
                 # apply dispersion
                 if SAVE_PLOTS:
                     vis.visualize_diffusion(city, date)
                 f.apply_diffusion_effect(city, roads, emptys)
                 if SAVE_PLOTS:
                     vis.visualize_diffusion(city, date)
-    
+
                 # calculate wind speed
-                if wind_speed_duration == 0 or sec%wind_speed_duration == 0:
-                    wind_speed_km, wind_speed_duration = f.calculate_wind_speed(f.sec_to(sec, "month")%12 + 1, sec)
-    
+                if wind_speed_duration == 0 or sec % wind_speed_duration == 0:
+                    wind_speed_km, wind_speed_duration = f.calculate_wind_speed(
+                        f.sec_to(sec, "month") % 12 + 1, sec)
+
                 # calculate wind direction
                 wind_direction = f.calculate_wind_directions(wind_speed_km)
-    
+
                 # convert wind_speed from km/h to m/s
                 wind_speed = float(wind_speed_km) * 1000 / 3600
-                #debug('wind_speed: ', wind_speed, "(m/sec)")
-                #debug('wind direction: ', wind_direction)
-    
+                print('wind_speed: ', wind_speed, "(m/sec)")
+                print('wind direction: ', wind_direction)
+
                 # calculate wind effect
                 if SAVE_PLOTS:
-                    vis.visualize_wind_effect(city, wind_speed, wind_direction, date)
-                f.apply_wind_effect(city, roads, emptys, wind_direction, wind_speed)
+                    vis.visualize_wind_effect(
+                        city, wind_speed, wind_direction, date)
+                f.apply_wind_effect(city, roads, emptys,
+                                    wind_direction, wind_speed)
                 if SAVE_PLOTS:
-                    vis.visualize_wind_effect(city, wind_speed, wind_direction, date)
-    
+                    vis.visualize_wind_effect(
+                        city, wind_speed, wind_direction, date)
+
                 # apply trees effect
                 if SAVE_PLOTS:
                     vis.visualize_trees_effect(city, date)
                 f.apply_trees_effect(city, trees)
                 if SAVE_PLOTS:
                     vis.visualize_trees_effect(city, date)
-    
+
                 # calculate rain effect
                 if SAVE_PLOTS:
                     vis.visualize_rain_effect(city, date)
                 rain_flag = f.rain(city)
                 if SAVE_PLOTS and rain_flag:
                     vis.visualize_rain_effect(city, date)
-    
-                print("co2 generated: ", f.calculate_co2(roads, emptys) - total_co2)
+
+                print("co2 generated: ", f.calculate_co2(
+                    roads, emptys) - total_co2)
                 total_co2 = f.calculate_co2(roads, emptys)
                 print("Total co2:", total_co2, "\n")
-    
+
             if sec % 60 == 0:
-                sensor_manager.measure(city, sec//60)
-    
-    
+                sensor_manager.measure(city, sec // 60)
+
             if sec % REAL_C02_PERIOD == 0:
-                #We shouldn't calculate the co2 in the roads+emptys, because we are only measuring on the roads.
-                #You can't compare an apple with a pear
-                co2_per_cell = f.calculate_co2(roads, []) / (len(roads))
+                # We calculate co2 in the entire 0 floor of the city, that means both the roads and the empty space
+                co2_per_cell = f.calculate_co2(
+                    roads, emptys_0) / (len(roads + emptys_0))
                 real_values.append(co2_per_cell / 125)
-    
-    
-            #get a measure
-            if sec % REAL_C02_PERIOD == 0:
-               # debug("Taking gateway measurement...")
+
+            # get a measure
+            if sec % SENSOR_PERIOD == 0:
+                print("Taking gateway measurement...")
                 measures = sensor_manager.gateway()
                 co2_per_sensor = np.sum(measures) / sensor_number
-                measured_values.append(co2_per_sensor/125)
+                measured_values.append(co2_per_sensor / 125)
+                real_co2_per_cell = f.calculate_co2(
+                    roads, emptys_0) / (len(roads + emptys_0))
+                data['measured_co2/cell'].append(co2_per_sensor)
+                data['real_co2/cell'].append(real_co2_per_cell)
+                data['timeframe'].append(SENSOR_PERIOD)
+                data['sec'].append(sec)
                 if not SENSOR_STATIC:
                     print("Moving sensors...")
                     sensor_manager.shuffle_sensors(roads)
-    
-    
+
             if sec >= TIME_TO_RUN:
                 print()
                 break
-    
-    
+
         # save a plot of co2 vs measured co2
         if SAVE_PLOTS:
-            vis.visualize_co2_comparison(co2=real_values, co2_measured=measured_values, duration=TIME_TO_RUN, frequency=REAL_C02_PERIOD)
-        # # calculate and print the total co2 in the city
-        # total_co2 = f.calculate_co2(roads)
-        # print("Total accumulated co2 in the city:", total_co2, "grams")
-        # total_measured_co2 = sensor_manager.get_total_co2()
-        # print("Total measured co2:", str(total_measured_co2), "grams")
-    
+            vis.visualize_co2_comparison(
+                co2=real_values, co2_measured=measured_values, duration=TIME_TO_RUN, frequency=SENSOR_PERIOD)
+
         score = calculation.calculate_error(real_values, measured_values)
-    
-        #Save results in csv file
-        newline =  [str(sensor_manager.get_sensor_cost()*sensor_number), str(SENSOR_DISTANCE), str(SENSOR_STATIC), str(REAL_C02_PERIOD), str(TIME_TO_RUN), str(round(score, 2))]
+
+        # Save results in csv file
+        newline = [str(sensor_manager.get_sensor_cost(TIME_TO_RUN) * sensor_number), str(SENSOR_DISTANCE),
+                   str(sensor_number), str(SENSOR_STATIC), str(SENSOR_PERIOD), str(TIME_TO_RUN), str(round(score, 4))]
         calculation.save_results(newline)
-    
-    
-    
+
+        # save data for simulation
+        df = pd.DataFrame.from_dict(data)
+        df.to_csv(os.path.join('figures', 'run_data',
+                               f'{TIME_TO_RUN}_{SENSOR_PERIOD}_{str(sensor_number)}_{SENSOR_STATIC}_{str(sensor_manager.get_sensor_cost(TIME_TO_RUN)*sensor_number)}.csv'), index=False)
+
         # after the simulation is done, visualize the co2 in the city
-        vis.visualize_co2(city, mesh=True, d=3, wind_direction=wind_direction, wind_speed=wind_speed, date=date)
-        vis.visualize_accuracy(real_values, REAL_C02_PERIOD, measured_values, REAL_C02_PERIOD)
+        vis.visualize_co2(
+            city, mesh=True, d=3, wind_direction=wind_direction, wind_speed=wind_speed, date=date)
+        vis.visualize_accuracy(real_values, REAL_C02_PERIOD,
+                               measured_values, SENSOR_PERIOD)
         # calculate and print the total co2 in the city
         total_co2 = f.calculate_co2(roads, emptys)
         print("Total accumulated co2 in the city:", total_co2, "grams")
         total_measured_co2 = sensor_manager.get_total_co2()
         print("Total measured co2:", str(total_measured_co2), "grams")
-    
-        score = calculation.calculate_error(real_values, measured_values)
-        print(f"Root-Mean-Square Error: {round(score, 2)}")
+
+        print(f"Root-Mean-Square Error: {round(score, 4)}")
         print(f"Sensors: {sensor_number}")
-    
-        print("Cost per device:", str(sensor_manager.get_sensor_cost()))
-        print("Total system cost:", str(sensor_manager.get_sensor_cost()*sensor_number))
-    
-        #Save results in csv file
-        newline =  [str(sensor_manager.get_sensor_cost()*sensor_number), str(SENSOR_DISTANCE), str(SENSOR_STATIC), str(REAL_C02_PERIOD), str(TIME_TO_RUN), str(round(score, 2))]
-        calculation.save_results(newline)
-    
+
+        print("Energy per device fro 1 year:", str(
+            sensor_manager.get_used_power(TIME_TO_RUN)), "mAh")
+        print("Cost per device:", str(
+            sensor_manager.get_sensor_cost(TIME_TO_RUN)))
+        print("Total system cost:", str(
+            sensor_manager.get_sensor_cost(TIME_TO_RUN) * sensor_number))
+
         # after the simulation is done, visualize the co2 in the city
-        vis.visualize_co2(city, mesh=True, d=3, wind_direction=wind_direction, wind_speed=wind_speed, date=date)
-        calculation.evaluate()
-        
-           
+        vis.visualize_co2(
+            city, mesh=True, d=3, wind_direction=wind_direction, wind_speed=wind_speed, date=date)
+        # calculation.evaluate()
 
 
 if __name__ == "__main__":
-
 
     main()
