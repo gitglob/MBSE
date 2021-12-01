39c4b260211b23044d531b10cd64965326ad8f11
gitglob <pgrigor22@gmail.com>
2021-12-01 19:05:01 +0100

    Update sensor.py

--- iot/sensor.py
+++ iot/sensor.py
@@ -2,12 +2,21 @@ import random
 
 
 class Sensor:
-    def __init__(self, error, range, cost, power):
+    N_MEASURES = 5
+    def __init__(self, error, cost, power):
         self.error = error
-        self.range = range
         self.cost = cost
         self.power = power
+        self.power_used = 0
 
     def measure(self, co2_value):
-        error = self.error*random.randrange(-1.0, 1.0)
-        return co2_value*(1 + error)
+        result = 0.0
+        for _ in range(self.N_MEASURES):
+            error = self.error*random.uniform(-1.0, 1.0)
+            result += co2_value*(1 + error)
+            self.power_used += self.power
+        result /= self.N_MEASURES
+        return result
+
+class SensorList:
+    SCD4 = Sensor(0.05, 7, 9.72e-3)
