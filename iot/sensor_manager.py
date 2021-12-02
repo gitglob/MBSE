e5e953cab342a324dd301c697869db2b7f35a23a
Vicente Bartual <vicentebartualf@gmail.com>
2021-12-02 19:23:18 +0100

    Place sensors on both sides of streets

--- iot/sensor_manager.py
+++ iot/sensor_manager.py
@@ -22,7 +22,7 @@ class SensorManager:
         for i in range(self.city.rows):
             for j in range(self.city.cols):
                 if ((self.city.city_model[i][j][0] == self.city.ROAD or self.city.city_model[i][j][0] == self.city.NOTHING) and valid_pos[i][j] == 1):
-                    self.add_sensor(i, j, 0)
+                    valid_pos[i][j] = 2
                     #mark positions around as invalid
                     bottom_x = max(0, j-distance-1)
                     bottom_y = max(0, i-distance-1)
@@ -30,7 +30,50 @@ class SensorManager:
                     top_y = min(self.city.rows, i + distance+1)
                     for y in range(bottom_y, top_y):
                         for x in range(bottom_x, top_x):
-                            valid_pos[y][x] = 0
+                            if valid_pos[y][x] != 2:
+                                valid_pos[y][x] = 0
+
+        if distance > 3:
+            pos = [(0, -1, 0, +2), (0, +1, 0, -2), (-1, 0, +2, 0), (+1, 0, -2, 0)]
+            change = False
+            for j in range(self.city.cols):
+                for i in range(self.city.rows):
+                    if valid_pos[i][j] == 2:
+                        for p in pos:
+                            if (i+p[0] < 0 or i+p[0] > self.city.rows-1
+                                    or j+p[1] < 0 or j+p[1] > self.city.cols-1
+                                    or self.city.city_model[i+p[0]][j+p[1]][0] == self.city.BUILDING
+                                    or self.city.city_model[i+p[0]][j+p[1]][0] == self.city.TREE):
+                                if change:
+                                    valid_pos[i][j] = 0
+                                    valid_pos[i+p[2]][j+p[3]] = 2
+                                    change = False
+                                else:
+                                    change = True
+                                break
+
+            for i in range(self.city.rows):
+                for j in range(self.city.cols):
+                    if valid_pos[i][j] == 2:
+                        for p in pos:
+                            if (i+p[0] < 0 or i+p[0] > self.city.rows-1
+                                    or j+p[1] < 0 or j+p[1] > self.city.cols-1
+                                    or self.city.city_model[i+p[0]][j+p[1]][0] == self.city.BUILDING
+                                    or self.city.city_model[i+p[0]][j+p[1]][0] == self.city.TREE):
+                                if change:
+                                    valid_pos[i][j] = 0
+                                    valid_pos[i+p[2]][j+p[3]] = 2
+                                    change = False
+                                else:
+                                    change = True
+                                break
+
+        for i in range(self.city.rows):
+            for j in range(self.city.cols):
+                if valid_pos[i][j] == 2:
+                    self.add_sensor(i, j, 0)
+
+
 
     def check_sensor_near(self, row, col, distance):
         for d in self.devices:
